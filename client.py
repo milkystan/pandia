1ad0c729e83de3baa679949a8a94822f8f5b8cbb
Milkystan <zfzfwan@163.com>
2017-03-20 15:49:11 +0800

    add .gitignore

---
+++ client.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/20 15:31
+# @Author  : Stan
+# @File    : client.py
---
+++ usage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/20 11:19
+# @Author  : Stan
+# @File    : usage.py
